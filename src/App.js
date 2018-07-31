7e64fe31d823420e226cf696108f5325cf116d42
Taylor Whipp <taylorpwhipp@gmail.com>
2018-07-31 13:59:26 -0700

    react router installed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
+/*import data from Google API here*/
 
 class App extends Component {
   render() {
