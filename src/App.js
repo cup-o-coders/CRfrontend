1b6165ac15155d9f05ce871e90152d7f1d49f656
learn <learn@learn09.local>
2018-08-01 14:19:31 -0700

    bug fix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Switch, Route,Link} from 'react-router-dom';
 import Home from './pages/Home';
 import Results from './pages/Results';
+import Header from './components/Header'
 import User from './pages/User';/*import data from Google API here*/
 
 class App extends Component {
