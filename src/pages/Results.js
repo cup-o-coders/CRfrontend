2d8834507dcd6c8944d8743274ee46d1e74b0543
learn <learn@learn09.local>
2018-08-02 13:32:38 -0700

    rough draft results page completed

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -9,16 +9,29 @@ class Results extends Component {
   render() {
     return (
       <div>
-        <div className="results-container">
+        <div className="flex-container">
           <div className="results-title">
             <h1>Your Matches</h1>
           </div>
-          <br />
           <div className="results-display">
             <p>Results go Here</p>
           </div>
           <div className="new-search">
             <input type="button" class="button" value="New Search" />
+          </div>
+          <div className="create">
+            <div className="create-intro">
+              <p>Create a User Profile to Save and Review Shops</p>
+            </div>
+            <div className="create-form">
+            <form action="/action_page.php">
+              <input type="text" placeholder="Name" name="Name" />
+              <input type="text" placeholder="Email" name="Email" />
+              <input type="text" placeholder="Password" name="Password" />
+              <br/>
+              <button type="submit">Create User</button>
+            </form>
+            </div>
           </div>
         </div>
       </div>
