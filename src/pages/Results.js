14436f1bc9a5a2ab96b267bb6a7d99f4efc75719
learn <learn@learn09.local>
2018-08-02 11:32:38 -0700

    created results page

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
-import {Switch, Route,Link} from 'react-router-dom'
+import {Switch, Route,Link} from 'react-router-dom';
+import '../css/results.css'
 /*import data from Google API here*/
 
 class Results extends Component {
   render() {
     return (
-        <h1>Hello</h1>
+      <div>
+        <div className="results-container">
+          <div className="results-title">
+            <h1>Your Matches</h1>
+          </div>
+          <br />
+          <div className="results-display">
+            <p>Results go Here</p>
+          </div>
+          <div className="new-search">
+            <input type="button" class="button" value="New Search" />
+          </div>
+        </div>
+      </div>
     );
   }
 }
