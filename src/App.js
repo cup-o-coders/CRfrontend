df0d26e82b73f433f848d279cc61a883dd025522
Brian Harrington <brianharrington2014@gmail.com>
2018-08-06 16:55:16 -0700

    API front end test working

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/Header'
 import User from './pages/User';/*import data from Google API here*/
 import Footer from './components/Footer.js'
 import Data from './data_sample/sample.json'
-import Yelptest from './api/Yelptest';
+import Yelptest from './api/Yelptest'
 
 
 class App extends Component {
@@ -16,10 +16,7 @@ class App extends Component {
     this.state= {data:Data.businesses}
   }
   render() {
-
-
     return (
-
       <div>
         <Header />
 
@@ -29,7 +26,7 @@ class App extends Component {
           <Route exact path="/" component={Home} />
          <Route path='/Results' render={(props)=><Results results={this.state.data}/>} />
           <Route path= '/User' component={User} />
-          <Route path='/yelptest' compoenent={ Yelptest } />
+          <Route path='/yelptest' component={ Yelptest } />
         </Switch>
 
       </div>
