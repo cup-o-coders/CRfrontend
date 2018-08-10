a5113c1d10011fedb6dae5a23ae0e8626f85cb13
Hunter Brown <hunterstokes@me.com>
2018-08-10 16:08:13 -0700

    text bubble above sign up form

--- src/components/createUser.js
+++ src/components/createUser.js
@@ -36,64 +36,63 @@ class createUser extends Component{
 
   render(){
     return(
-      <div className="container">
-        <div className="login-hero">
-          <img src={require("../images/cup.jpg")} />
-        </div>
-        <div className="signup-intro">
-            Enter your information to signup!
-        </div>
-        <div className="login-form-container">
+        <div className="container">
+            <div className="login-hero">
+                <img src={require("../images/cup.jpg")} />
+            </div>
+            <div className="login-form-container">
+                <article>
+                    <p>Enter your information to signup!</p>
+                </article>
             <form onSubmit={this.submitNewUser.bind(this)}>
-              <ul className ="flex-outer">
-                <li className="name-label">
-                  <label>Name: </label>
-                  <input
-                  className="form-item"
-                  placeholder="Name"
-                  name="name"
-                  type="text"
-                  onChange={this.handleChange.bind(this)}
-                  value={this.state.name}
-                  />
-                </li>
-                <li className="email-label">
-                  <label>Email: </label>
-                  <input
-                  className="form-item"
-                  placeholder="Email"
-                  name="email"
-                  type="text"
-                  onChange={this.handleChange.bind(this)}
-                  value={this.state.email}
-                  />
-                </li>
-                <li className="password-label">
-                  <label>Password: </label>
-                  <input
-                  className="form-item"
-                  placeholder="Password"
-                  name="password"
-                  type="password"
-                  onChange={this.handleChange.bind(this)}
-                  value={this.state.password}
-                  />
-                </li>
-                <li className="confirm-label">
-                  <label>Password Confirmation: </label>
-                  <input
-                  className="form-item"
-                  placeholder="Confirm Password"
-                  name="password_confirmation"
-                  type="password"
-                  onChange={this.handleChange.bind(this)}
-                  value={this.state.password_confirmation}
-                  />
-                </li>
-                <li>
-                 <input type="submit" value="Submit" />
-               </li>
-              </ul>
+                <ul className ="flex-outer">
+                    <li className="name-label">
+                        <label>Name: </label>
+                        <input
+                        className="form-item"
+                        placeholder="Name"
+                        name="name"
+                        type="text"
+                        onChange={this.handleChange.bind(this)}
+                        value={this.state.name}
+                    />
+                    </li>
+                    <li className="email-label">
+                        <label>Email: </label>
+                        <input
+                        className="form-item"
+                        placeholder="Email"
+                        name="email"
+                        type="text"
+                        onChange={this.handleChange.bind(this)}
+                        value={this.state.email}/>
+                    </li>
+                    <li className="password-label">
+                        <label>Password: </label>
+                        <input
+                        className="form-item"
+                        placeholder="Password"
+                        name="password"
+                        type="password"
+                        onChange={this.handleChange.bind(this)}
+                        value={this.state.password}
+                        />
+                    </li>
+                    <li className="confirm-label">
+                        <label>Password Confirmation: </label>
+                        <input
+                        className="form-item"
+                        placeholder="Confirm Password"
+                        name="password_confirmation"
+                        type="password"
+                        onChange={this.handleChange.bind(this)}
+                        value={this.state.password_confirmation}
+                        />
+                    </li>
+                    <li>
+                        <input type="submit" value="Submit" />
+                    </li>
+                </ul>
             </form>
         </div>
     </div>
